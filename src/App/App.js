f11e83e9c43b49cf5f639094bf5d3296a169d65b
kevindongzg <dongkevin94@gmail.com>
2020-11-02 21:58:08 +0800

    add TODOs

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+// TODO GTB-1: * 页面样式基本上没有，只有一定层度的布局
+// TODO GTB-1: * 实现分组列表 学员列表功能，但刷新后没有显示分组列表
+// TODO GTB-2: * 没有测试
+// TODO GTB-3: * 没有做组件拆分，只有一个App组件，思考如何拆分与复用组件
+// TODO GTB-3: * 没有使用语义化标签
+// TODO GTB-3: * 没有使用flex，写了scss及样式但没有用到什么知识点，也没有用到scss任何特性
+// TODO GTB-3: * 运用了ES6+语法及fetch
+// TODO GTB-3: * 运用React相关知识点，但未拆分组件，一些知识点无法验证
+// TODO GTB-4: * 有小步提交意识，但提交可以更小步，如拆分组件后以组件粒度等
+// TODO GTB-4: * 没有抽出Api请求层
+// TODO GTB-4: * 没有做组件拆分，App组件过长
+// TODO GTB-4: * 没有组件拆分所以scss也没有拆分
+// TODO GTB-4: * 分组数据设计有点问题，多少个组是应该是后端告诉你的
 class App extends Component {
+  // TODO GTB-4: - 不要滥用eslint disable功能，要自己动手试试改eslint的error
   // eslint-disable-next-line react/state-in-constructor
   state = {
     // eslint-disable-next-line react/no-unused-state
     users: [],
+    // TODO GTB-4: - 不应该写死6个组，应该由后端返回给你
     // eslint-disable-next-line react/no-unused-state
     group1: [],
     // eslint-disable-next-line react/no-unused-state
@@ -21,6 +36,7 @@ class App extends Component {
   };
 
   componentDidMount() {
+    // TODO GTB-4: - 可以抽出API请求层，解耦请求与渲染
     fetch(`http://localhost:8080/users`, {
       method: 'get',
     })
@@ -54,24 +70,30 @@ class App extends Component {
       });
   }
 
+  // TODO GTB-4: - 未拆分组件，导致下面jsx嵌套过深，可读性较差
   render() {
     return (
+      // TODO GTB-3: - 加强语义化标签的使用
       <div data-testid="app" className="App">
+        {/* TODO GTB-4: - 标签class命名格式约定为a-b-c */}
         <div className="app_title">
           <span className="app_title">分组列表</span>
           <input
             className="spilt_button"
             type="button"
             value="分组学员"
+            // TODO GTB-3: - 这里可以直接写{this.handleClick}，定义那里改成箭头函数
             onClick={() => this.handleClick()}
           />
         </div>
+        {/* TODO GTB-4: - 组数不是死的，而是后端返回的 */}
         {this.state.group1.length > 0 && (
           <div>
             <div>1 组</div>
             <div className="button_div">
               {this.state.group1.map((user, index) => {
                 return (
+                  // TODO GTB-4: - 一般不推荐使用index作为key，可以使用id等
                   <input
                     className="name_button"
                     type="button"
